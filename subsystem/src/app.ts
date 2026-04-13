e2f9754b8d8d0eda21c50842c85fae0545d5f8b1
LuisGrigore <LuisGrigore@users.noreply.github.com>
2026-04-13 14:01:18 +0200

    renaming

--- subsystem/src/app.ts
+++ subsystem/src/app.ts
@@ -1,11 +1,14 @@
 import { GodotEventBus } from "./EventBus";
 
 const onMessage = (message: unknown) => {
-  const messageObj = message as { content: String };
+  const messageObj = message as { content: string };
   console.log(messageObj.content);
 };
 
 export const startApp = async () => {
   const bus = new GodotEventBus();
+  document.addEventListener("keydown", (event) => {
+	bus.sendEventToGodot("input", event.key)
+  });
   bus.onEventFromGodot("Message", onMessage);
 };
