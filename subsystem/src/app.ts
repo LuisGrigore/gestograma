ed38f1341e8b438935fb00c21fad5d38fe00b458
LuisGrigore <LuisGrigore@users.noreply.github.com>
2026-04-13 19:40:59 +0200

    mediapipe integration optimized

--- subsystem/src/app.ts
+++ subsystem/src/app.ts
@@ -13,44 +13,71 @@ export const startApp = async () => {
   video.playsInline = true;
 
   const vision = await FilesetResolver.forVisionTasks(
-    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm",
   );
 
   const handLandmarker = await HandLandmarker.createFromOptions(vision, {
     baseOptions: {
       modelAssetPath:
         "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task",
+      delegate: "GPU", // 🔥 CLAVE
     },
     runningMode: "VIDEO",
     numHands: 2,
   });
-
   console.log("Hands model loaded");
+
+  // -----------------------------
+  // FPS tracking
+  // -----------------------------
+  //   let frameCount = 0;
+  //   let lastFpsTime = performance.now();
 
   // -----------------------------
   // Detection loop
   // -----------------------------
   let lastTime = 0;
+  let isProcessing = false;
+
   const FPS = 30;
   const interval = 1000 / FPS;
+
+  let frameCount = 0;
+  let lastFpsTime = performance.now();
 
   const detectHands = (time: number = 0) => {
     requestAnimationFrame(detectHands);
 
+    // evitar saturación (no lanzar otra inferencia si sigue procesando)
+    if (isProcessing) return;
+
+    // control de FPS
     if (time - lastTime < interval) return;
     lastTime = time;
 
+    isProcessing = true;
+
     const results = handLandmarker.detectForVideo(video, time);
 
-    // ✅ SOLO enviar si hay detección real
+    isProcessing = false;
+
+    // 📊 contar inferencias reales
+    frameCount++;
+
+    // 📈 calcular FPS cada segundo
+    if (time - lastFpsTime >= 1000) {
+      const fps = frameCount;
+      console.log("Inference FPS:", fps);
+
+      frameCount = 0;
+      lastFpsTime = time;
+    }
+
+    // 🖐️ enviar solo si hay manos
     const hands = results.landmarks ?? [];
 
     if (hands.length > 0) {
-      bus.sendEventToGodot("hands", {
-        hands,
-        world: results.worldLandmarks ?? [],
-        timestamp: time,
-      });
+      bus.sendEventToGodot("hands", hands);
     }
   };
 
@@ -60,8 +87,8 @@ export const startApp = async () => {
   const startCamera = async () => {
     const stream = await navigator.mediaDevices.getUserMedia({
       video: {
-        width: 640,
-        height: 480,
+        width: 320,
+        height: 240,
       },
     });
 
